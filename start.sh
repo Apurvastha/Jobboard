ee004bbfdf9e8d0f5062f8979e7543b4e75831a5
Reaper <69500690+Apurvastha@users.noreply.github.com>
2026-06-30 03:27:00 +0545

    temporary purge command in start.sh for celery backlog purge

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+celery -A jobboard purge -f || true
 celery -A jobboard worker --loglevel=info --concurrency=2 &
 CELERY_PID=$!
 gunicorn jobboard.wsgi:application --bind 0.0.0.0:$PORT --workers 2
