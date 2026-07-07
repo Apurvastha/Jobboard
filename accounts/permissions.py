27f2efd02e27e90dcd6d8c87c729e4d9ce017020
Reaper <69500690+Apurvastha@users.noreply.github.com>
2026-07-07 23:30:58 +0545

    fix typo in accounts/permissions.py

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -73,7 +73,7 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        return request.user.is_authticated and request.user.is_admin
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsApplicationOwner(BasePermission):
